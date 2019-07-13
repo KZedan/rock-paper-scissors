8cebe30c0274f603be72be520a6596062ceae614
Kareem <kareem335@yahoo.co.uk>
2019-07-13 21:55:37 +0100

    finished finished

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require_relative 'lib/app'
+require 'lib/app.rb'
 run RockPaperScissors
--- lib/app.rb
+++ lib/app.rb
@@ -3,14 +3,25 @@ require 'shotgun'
 require_relative "score_decider"
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
   set :session_secret, 'super secret'
 
   get '/' do
     erb :index
   end
 
-  post '/entries' do
-    $score = ScoreDecider.new(params[:player_1_move], params[:player_2_move])
+  post '/entry_1' do
+    session[:player_1_move] = params[:player_1_move]
+    redirect '/submit_2'
+  end
+
+  get '/submit_2' do
+    erb :player_2_move
+  end
+
+  post '/entry_2' do
+    session[:player_2_move] = params[:player_2_move]
+    $score = ScoreDecider.new(session[:player_1_move], session[:player_2_move])
     redirect '/score'
   end
 
@@ -18,7 +29,6 @@ class RockPaperScissors < Sinatra::Base
     @score = $score.decider
     erb :details
   end
-
 
   run! if app_file == $0
 end
